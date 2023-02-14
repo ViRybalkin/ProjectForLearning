6dc71ed4ebcc429a7794848159e822a0967b1ffc
Vitaliy Rybalkin <kinjeks@gmail.com>
2023-02-14 21:02:55 +0300

    feat: Добавлен дефолтный браузер для дев сервера

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -3,6 +3,10 @@ import type { Configuration as DevServerConfiguration } from "webpack-dev-server
 export const buildDevServer = (port: number): DevServerConfiguration => {
     return {
         port: port,
-        open: true,
+        open: {
+            app: {
+                name: 'Safari'
+            }
+        },
     }
 }
