b87d8c02023b0879fae4bb2cdf4daccd7f56e7f6
Vitaliy Rybalkin <kinjeks@gmail.com>
2023-02-14 21:02:55 +0300

    feat: Добавлен базовый роутинг

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -3,6 +3,7 @@ import type { Configuration as DevServerConfiguration } from "webpack-dev-server
 export const buildDevServer = (port: number): DevServerConfiguration => {
     return {
         port: port,
+        historyApiFallback: true,
         open: {
             app: {
                 name: 'Safari'
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,24 @@
-import Counter from "../components/Counter/Counter";
 import styles from '../index.module.scss'
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import MainPage from "../pages/MainPage/MainPage";
+import {lazy, Suspense} from "react";
 
 export const App = () => {
+    const MainPage = lazy(() => import('../pages/MainPage/MainPage'));
+    const AboutPage = lazy(() => import('../pages/About/About'));
+
     return (
-        <div className={styles.app}>
-          <Counter/>
-        </div>
+        <BrowserRouter>
+            <div className={styles.app}>
+                <Link to={'/'} > Main Page</Link>
+                <Link to={'/about'}>About Page</Link>
+                    <Suspense fallback={<div>Loading ...</div>}>
+                        <Routes>
+                            <Route element={<MainPage/>} path={'/'} />
+                            <Route element={<AboutPage/>} path={'/about'} />
+                        </Routes>
+                    </Suspense>
+            </div>
+        </BrowserRouter>
     )
-}
+};
