269e4c92803a418e1203e451debdf9f8e06b4db5
Vitaliy Rybalkin <kinjeks@gmail.com>
2023-02-17 22:43:44 +0300

    config: Добавлены типы комитов

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [2, 'always', 72],
+    'type-enum': [2, 'always', ['chore', 'docs', 'feat', 'fix', 'config', 'refactor', 'style', 'test']],
   },
 };
