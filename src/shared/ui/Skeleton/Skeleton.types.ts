62a69b67a914ec4b9c562083f692382b6fcd3620
Vitaliy Rybalkin <kinjeks@gmail.com>
2023-04-04 18:18:36 +0300

    feat: Добавлен компонент скелетона

---
+++ src/shared/ui/Skeleton/Skeleton.types.ts
@@ -0,0 +1,7 @@
+import { Property } from 'csstype';
+
+export interface SkeletonProps {
+  width: Property.Width<string | number>;
+  height: Property.Height<string | number>;
+  borderRadius?: Property.BorderRadius<string | number>;
+}
