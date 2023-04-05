f05005a83d263b14103be0d7118997b0834143d0
Vitaliy Rybalkin <kinjeks@gmail.com>
2023-04-05 11:24:35 +0300

    feat: Добавлены состояния ошибки и загрузки + hover эффект кнопки

--- src/shared/ui/Skeleton/Skeleton.tsx
+++ src/shared/ui/Skeleton/Skeleton.tsx
@@ -3,12 +3,12 @@ import { classNames } from 'app';
 import cls from './Skeleton.module.scss';
 import { SkeletonProps } from './Skeleton.types';
 
-export const Skeleton = memo(({ height, width, borderRadius }: SkeletonProps) => {
+export const Skeleton = memo(({ height, width, borderRadius, className }: SkeletonProps) => {
   const style: CSSProperties = {
     width,
     height,
     borderRadius,
   };
 
-  return <div style={style} className={classNames(cls.skeleton)} />;
+  return <div style={style} className={classNames(cls.skeleton, {}, [className])} />;
 });
