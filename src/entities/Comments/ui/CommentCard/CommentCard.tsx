6d175bcc5a22c49139accf932574fa5f3c88a07b
Рыбалкин Виталий Михайлович <vmrybalkin@phoenixit.ru>
2023-04-27 20:03:05 +0300

    feat: Добавлен переход к профилю пользователя оставившего комментарий

--- src/entities/Comments/ui/CommentCard/CommentCard.tsx
+++ src/entities/Comments/ui/CommentCard/CommentCard.tsx
@@ -1,15 +1,25 @@
-import React, { memo } from 'react';
-import { Avatar, Typography } from 'shared';
+import React, { memo, useCallback } from 'react';
+import { Avatar, Button, Typography } from 'shared';
+import { useNavigate } from 'react-router-dom';
+import { routerPath } from 'shared/config/routes/Routes';
 import { CommentCardProps } from './CommentCard.types';
 import cls from './CommentCard.module.scss';
 
 export const CommentCard = memo(({ comment }: CommentCardProps) => {
+  const navigate = useNavigate();
+
+  const onUserClick = useCallback(() => {
+    navigate(`${routerPath.profile}${comment.user?.id}`);
+  }, [comment.user?.id, navigate]);
+
   return (
     <div className={cls.commentWrapper}>
-      <div className={cls.commentWrapper__userInfo}>
-        {comment.user?.avatar ? <Avatar src={comment.user?.avatar} alt={comment.user?.avatar} size={30} /> : null}
-        <Typography>{comment.user?.username}</Typography>
-      </div>
+      <Button onClick={onUserClick} className={cls.buttonColor} theme='clear'>
+        <div className={cls.commentWrapper__userInfo}>
+          {comment.user?.avatar ? <Avatar src={comment.user?.avatar} alt={comment.user?.avatar} size={30} /> : null}
+          <Typography>{comment.user?.username}</Typography>
+        </div>
+      </Button>
       <Typography>{comment.comment}</Typography>
     </div>
   );
