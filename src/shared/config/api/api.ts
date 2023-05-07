a6b99bdb56476ba277c63713e8399d6581bfdbc3
ViRybalkin <kinjeks@gmail.com>
2023-05-07 12:59:36 +0300

    fix: исправлен интерцептор для апи

--- src/shared/config/api/api.ts
+++ src/shared/config/api/api.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
+import {LOCAL_STORAGE_KEY} from "shared";
 
 export const $api = axios.create({
-  baseURL: __BASE_URL__,
-  headers: {
-    authorization: localStorage.getItem('auth') || ''
-  }
+    baseURL: __BASE_URL__,
+})
+$api.interceptors.request.use(config => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(LOCAL_STORAGE_KEY.auth) || '';
+    }
+    return config;
 })
