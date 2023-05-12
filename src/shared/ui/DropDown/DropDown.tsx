80da5104ac86eda7a05650ded9668c8d5ba41452
ViRybalkin <kinjeks@gmail.com>
2023-05-12 20:58:26 +0300

    feat: Добавлен проп с позицией меню dropDown'a

--- src/shared/ui/DropDown/DropDown.tsx
+++ src/shared/ui/DropDown/DropDown.tsx
@@ -5,11 +5,11 @@ import { classNames } from 'app';
 import { DropDownProps } from './DropDown.types';
 import cls from './DropDown.module.scss';
 
-export const Dropdown = memo(({ items, buttonItem }: DropDownProps) => {
+export const Dropdown = memo(({ items, buttonItem, position = 'bottomLeft' }: DropDownProps) => {
   return (
     <Menu as='div' className={cls.dropdown}>
       <Menu.Button className={cls.button}>{buttonItem}</Menu.Button>
-      <Menu.Items className={cls.items}>
+      <Menu.Items className={classNames(cls.items, {}, [cls[position]])}>
         {items.map((item) => {
           const content = ({ active }: { active: boolean }) => (
             <button className={classNames(cls.item, { [cls.active]: active })} onClick={item.onClick}>
@@ -18,7 +18,7 @@ export const Dropdown = memo(({ items, buttonItem }: DropDownProps) => {
           );
           if (item.href) {
             return (
-              <Menu.Item disabled={item.disabled} as={AppLink} to={item.href} className={cls.item}>
+              <Menu.Item disabled={item.disabled} as={AppLink} to={item.href}>
                 {content}
               </Menu.Item>
             );
