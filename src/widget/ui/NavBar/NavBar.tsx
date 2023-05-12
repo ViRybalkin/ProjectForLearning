1b4083d38d2f72fd2fec08be318c67ed20ff07f3
ViRybalkin <kinjeks@gmail.com>
2023-05-12 21:01:01 +0300

    refactor: кнопки пользователя заменены на dropDown

--- src/widget/ui/NavBar/NavBar.tsx
+++ src/widget/ui/NavBar/NavBar.tsx
@@ -1,5 +1,5 @@
 import { classNames, useAppDispatch } from 'app';
-import { Button, CustomLink, HStack } from 'shared';
+import { Avatar, Button, CustomLink, Dropdown, HStack } from 'shared';
 import { ThemeSwitcher } from 'widget';
 import { useTranslation } from 'react-i18next';
 import { memo, useCallback, useState } from 'react';
@@ -11,7 +11,7 @@ import { routerPath } from 'shared/config/routes/Routes';
 import { NavBarProps } from './NavBar.types';
 import cls from './navBar.module.scss';
 
-const NavBar = memo(({ className }: NavBarProps) => {
+export const NavBar = memo(({ className }: NavBarProps) => {
   const [open, setOpen] = useState<boolean>(false);
   const navigate = useNavigate();
   const isAuth = useSelector(getIsAuth);
@@ -27,34 +27,38 @@ const NavBar = memo(({ className }: NavBarProps) => {
     navigate(routerPath.main);
   }, [dispatch, navigate]);
 
-  const { t } = useTranslation(['links', 'translation']);
+  const { t } = useTranslation(['links', 'translation', 'profilePage']);
 
   return (
     <>
       <header data-testid='navBarId' className={classNames(cls.navBar, {}, [className])}>
         <ThemeSwitcher />
-        <HStack>
-          <HStack data-testid='navTagNavBarId'>
-            <CustomLink to='/' name={t('main')} />
-            <CustomLink to='/about' name={t('about')} />
-            {isAuth ? <CustomLink to={`profile/${user?.id}`} name={t('profile')} /> : null}
-            {isAuth ? <CustomLink to='/articles' name={t('articles')} /> : null}
-          </HStack>
-          {!isAuth ? (
+        <HStack data-testid='navTagNavBarId'>
+          <CustomLink to='/' name={t('main')} />
+          <CustomLink to='/about' name={t('about')} />
+          {isAuth ? <CustomLink to='/articles' name={t('articles')} /> : null}
+          {isAuth ? (
+            <Dropdown
+              buttonItem={<Avatar size={40} alt={user?.avatar} src={user?.avatar} />}
+              items={[
+                {
+                  content: t('profileCardTitle', { ns: 'profilePage' }),
+                  href: `profile/${user?.id}`,
+                },
+                {
+                  content: t('logout', { ns: 'translation' }),
+                  onClick: onLogout,
+                },
+              ]}
+            />
+          ) : (
             <Button data-testid='loginBtnId' onClick={onLogin}>
               {t('login', { ns: 'translation' })}
-            </Button>
-          ) : (
-            <Button data-testid='logoutBtnId' onClick={onLogout}>
-              {t('logout', { ns: 'translation' })}
             </Button>
           )}
         </HStack>
       </header>
-
       {!isAuth ? <LoginModal isOpen={open} onClose={() => setOpen(false)} /> : null}
     </>
   );
 });
-
-export { NavBar };
