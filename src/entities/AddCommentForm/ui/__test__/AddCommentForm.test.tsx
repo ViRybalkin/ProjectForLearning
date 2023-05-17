4bb5a4a8f9578493db1f1e4bd064a6ec3607f61a
ViRybalkin <kinjeks@gmail.com>
2023-05-17 12:38:46 +0300

    chore: Добавлен ключ isolatedModules

--- src/entities/AddCommentForm/ui/__test__/AddCommentForm.test.tsx
+++
@@ -1,3 +0,0 @@
-describe('Тестирование AddCommentForm', () => {
-    test('')
-});
--- src/entities/Profile/config/service/ValidationErrors/__test__/ValidationErrors.service.test.ts
+++ src/entities/Profile/config/service/ValidationErrors/__test__/ValidationErrors.service.test.ts
@@ -1,61 +1,63 @@
 import {validateProfile} from "../ValidationErrors.service";
 
 describe('Тестирование сервиса ValidationErrors', () => {
-  test('Если не переданы имя или фамилия сервис должен вернуть корректные данные', () => {
-    const profile = {
-      first: '',
-      lastname: '',
-      currency: 'currency',
-      country: 'country',
-      city: 'city',
-      username: 'username',
-      avatar: 'avatar',
-      age: 22,
-    }
+    test('Если не переданы имя или фамилия сервис должен вернуть корректные данные', () => {
+        const profile = {
+            id: '',
+            first: '',
+            lastname: '',
+            currency: 'currency',
+            country: 'country',
+            city: 'city',
+            username: 'username',
+            avatar: 'avatar',
+            age: 22,
+        }
 
-    const result = validateProfile(profile)
+        const result = validateProfile(profile)
 
-    expect(result).toEqual(['INCORRECT_USER_DATA'])
-  })
+        expect(result).toEqual(['INCORRECT_USER_DATA'])
+    })
 
-  test('Если не переданы возраст сервис должен вернуть корректные данные', () => {
-    const profile = {
-      first: 'first',
-      lastname: 'lastname',
-      currency: 'currency',
-      country: 'country',
-      city: 'city',
-      username: 'username',
-      avatar: 'avatar',
-      age: undefined,
-    }
+    test('Если не переданы возраст сервис должен вернуть корректные данные', () => {
+        const profile = {
+            id: '1',
+            first: 'first',
+            lastname: 'lastname',
+            currency: 'currency',
+            country: 'country',
+            city: 'city',
+            username: 'username',
+            avatar: 'avatar',
+            age: undefined,
+        }
 
-    const result = validateProfile(profile)
+        const result = validateProfile(profile)
 
-    expect(result).toEqual(['INCORRECT_AGE'])
-  })
+        expect(result).toEqual(['INCORRECT_AGE'])
+    })
 
-  test('Если не переданы возраст сервис должен вернуть корректные данные', () => {
-    const profile = {
-      first: 'first',
-      lastname: 'lastname',
-      currency: 'currency',
-      city: 'city',
-      username: 'username',
-      avatar: 'avatar',
-      age: 22,
-    }
+    test('Если не переданы возраст сервис должен вернуть корректные данные', () => {
+        const profile = {
+            first: 'first',
+            lastname: 'lastname',
+            currency: 'currency',
+            city: 'city',
+            username: 'username',
+            avatar: 'avatar',
+            age: 22,
+        }
 
-    // @ts-ignore
-    const result = validateProfile(profile)
+        // @ts-ignore
+        const result = validateProfile(profile)
 
-    expect(result).toEqual(['INCORRECT_COUNTRY'])
-  })
+        expect(result).toEqual(['INCORRECT_COUNTRY'])
+    })
 
-  test('Если не переданы профиль сервис должен вернуть корректные данные', () => {
-    // @ts-ignore
-    const result = validateProfile(undefined)
+    test('Если не переданы профиль сервис должен вернуть корректные данные', () => {
+        // @ts-ignore
+        const result = validateProfile(undefined)
 
-    expect(result).toEqual(["NO_DATA", "INCORRECT_USER_DATA", "INCORRECT_AGE", "INCORRECT_COUNTRY"])
-  })
+        expect(result).toEqual(["NO_DATA", "INCORRECT_USER_DATA", "INCORRECT_AGE", "INCORRECT_COUNTRY"])
+    })
 })
