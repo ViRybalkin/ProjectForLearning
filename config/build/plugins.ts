e16fa8de4f644acf97d3f0aef1e1ee6de0148abd
ViRybalkin <kinjeks@gmail.com>
2023-05-18 17:01:13 +0300

    chore: Заменен тайпскрипт лоадер на babel и добавлена проверка типов

--- config/build/plugins.ts
+++ config/build/plugins.ts
@@ -5,6 +5,7 @@ import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 import CopyPlugin from 'copy-webpack-plugin';
 import CircularDependencyPlugin from 'circular-dependency-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import {WebpackBuildOption} from "./types";
 
 
@@ -44,6 +45,7 @@ export const Plugins = ({
 
     if (isDev) {
         plugins.push(new ReactRefreshWebpackPlugin())
+        plugins.push(new ForkTsCheckerWebpackPlugin())
     }
 
     return plugins;
