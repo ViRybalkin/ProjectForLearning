e10dcd8ce5fa9d9ac5fb0d066a8aade4c52bbf40
ViRybalkin <kinjeks@gmail.com>
2023-05-25 17:35:14 +0300

    feat: Добавлен свайп для drawer

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,33 +1,76 @@
-import React, { memo } from 'react';
-import { useModal } from 'shared/config/helpers/useModal';
+import React, { memo, useCallback, useEffect } from 'react';
+import { a, config, useSpring } from '@react-spring/web';
+import { useDrag } from '@use-gesture/react';
 import { DrawerProps } from './Drawer.types';
 import cls from './Drawer.module.scss';
 import { Portal } from '../Portal/Portal';
 import { classNames } from '../../config/helpers/classNames';
 import { Overlay } from '../Overlay';
 
-export const Drawer = memo(
-  ({ isOpen, onClose, children, onEscapeClose = true, onOverlayClose = true, lazy = false }: DrawerProps) => {
-    const { onOverlayClick, onContentClick, isMounted } = useModal({
-      onClose,
-      isOpen,
-      onEscapeClose,
-      onOverlayClose,
-    });
+const height = window.innerHeight - 100;
 
-    if (lazy && !isMounted) {
-      return null;
+export const Drawer = memo(({ isOpen, onClose, children }: DrawerProps) => {
+  const [{ y }, api] = useSpring(() => ({ y: height }));
+
+  const openDrawer = useCallback(() => {
+    api.start({ y: 0, immediate: false });
+  }, [api]);
+
+  useEffect(() => {
+    if (isOpen) {
+      openDrawer();
     }
-    return (
-      <Portal>
-        <div data-testid='modalTestId' className={classNames(cls.drawer, { [cls.opened]: isOpen })}>
-          <Overlay testId='overlayTestId' onClick={onOverlayClick} classname={cls.overlay}>
-            <div onClick={onContentClick} className={classNames(cls.content)}>
-              {children}
-            </div>
-          </Overlay>
-        </div>
-      </Portal>
-    );
+  }, [api, isOpen, openDrawer]);
+
+  const close = (velocity = 0) => {
+    api.start({
+      y: height,
+      immediate: false,
+      config: { ...config.stiff, velocity },
+      onResolve: onClose,
+    });
+  };
+
+  const bind = useDrag(
+    ({ last, velocity: [, vy], direction: [, dy], movement: [, my], cancel }) => {
+      if (my < -70) cancel();
+
+      if (last) {
+        if (my > height * 0.5 || (vy > 0.5 && dy > 0)) {
+          close();
+        } else {
+          openDrawer();
+        }
+      } else {
+        api.start({ y: my, immediate: true });
+      }
+    },
+    {
+      from: () => [0, y.get()],
+      filterTaps: true,
+      bounds: { top: 0 },
+      rubberband: true,
+    }
+  );
+
+  const display = y.to((py) => (py < height ? 'block' : 'none'));
+
+  if (!isOpen) {
+    return null;
   }
-);
+
+  return (
+    <Portal>
+      <div data-testid='modalTestId' className={classNames(cls.drawer, { [cls.opened]: isOpen })}>
+        <Overlay testId='overlayTestId' onClick={() => close()} classname={cls.overlay}>
+          <a.div
+            style={{ display, bottom: `calc(-100vh + ${height - 100}px)`, y }}
+            className={classNames(cls.content)}
+            {...bind()}>
+            {children}
+          </a.div>
+        </Overlay>
+      </div>
+    </Portal>
+  );
+});
