6e4467e9b1c117528e0e89e805164abf28e95729
ViRybalkin <kinjeks@gmail.com>
2023-05-30 18:21:34 +0300

    feat: Добавлен компонент оценки статьи

---
+++ src/entities/Rating/ui/Rating/Rating.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Rating } from './Rating';
+
+export default {
+  title: 'entities/Rating',
+  component: Rating,
+} as ComponentMeta<typeof Rating>;
+
+export const Template: ComponentStory<typeof Rating> = (args) => <Rating {...args} />;
