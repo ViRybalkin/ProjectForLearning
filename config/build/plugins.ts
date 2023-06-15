9bff8ab9b24be19b713421c7016e3fba72fdf582
ViRybalkin <kinjeks@gmail.com>
2023-06-15 17:35:20 +0300

    chore: Поправлена загрузка плаигнов вебпака и кэширование

--- config/build/plugins.ts
+++ config/build/plugins.ts
@@ -3,9 +3,9 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
-import CopyPlugin from 'copy-webpack-plugin';
 import CircularDependencyPlugin from 'circular-dependency-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import CopyPlugin from "copy-webpack-plugin";
 import {WebpackBuildOption} from "./types";
 
 
@@ -18,13 +18,8 @@ export const Plugins = ({
                         }: WebpackBuildOption): webpack.WebpackPluginInstance[] => {
 
     const plugins = [
-
         new HtmlWebpackPlugin({template: path.html}),
         new webpack.ProgressPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
-        }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
             __BASE_URL__: JSON.stringify(baseUrl),
@@ -34,8 +29,6 @@ export const Plugins = ({
             openAnalyzer: isReport,
             analyzerMode: isReport ? 'server' : 'disabled',
         }),
-        new CopyPlugin({patterns: [{from: path.fromLocale, to: path.toLocale}]}),
-
         new CircularDependencyPlugin({
             exclude: /node_modules/,
             failOnError: true,
@@ -46,6 +39,13 @@ export const Plugins = ({
     if (isDev) {
         plugins.push(new ReactRefreshWebpackPlugin())
         plugins.push(new ForkTsCheckerWebpackPlugin())
+    }
+    if (!isDev) {
+        plugins.push(new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }))
+        plugins.push(new CopyPlugin({patterns: [{from: path.fromLocale, to: path.toLocale}]}),)
     }
 
     return plugins;
