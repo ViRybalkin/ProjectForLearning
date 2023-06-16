a97648dae92e2368360b9d9596ab4536a8ae1e57
ViRybalkin <kinjeks@gmail.com>
2023-06-16 13:05:36 +0300

    feat: Добавлен компонент для обработки изображений

---
+++ src/shared/ui/AppImage/AppImage.tsx
@@ -0,0 +1,32 @@
+import { memo, useLayoutEffect, useState } from 'react';
+import { AppImageProps } from './AppImage.types';
+import { classNames } from '@/shared/helpers/classNames';
+
+export const AppImage = memo(
+  ({ src, classname, fallback, errorFallback, alt = 'image', ...otherProps }: AppImageProps) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+
+    useLayoutEffect(() => {
+      const img = new Image();
+      img.src = src ?? '';
+      img.onload = () => {
+        setIsLoading(false);
+      };
+      img.onerror = () => {
+        setIsLoading(false);
+        setIsError(true);
+      };
+    }, [src]);
+
+    if (isLoading && fallback) {
+      return fallback;
+    }
+
+    if (isError && errorFallback) {
+      return errorFallback;
+    }
+
+    return <img className={classNames('', {}, [classname])} src={src} alt={alt} {...otherProps} />;
+  }
+);
