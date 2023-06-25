3b28598d548fbd2012052ddf76f46a59ac294246
ViRybalkin <kinjeks@gmail.com>
2023-06-25 14:55:27 +0300

    chore: Обновлены пакеты сторибука

--- docs/storybook.md
+++ docs/storybook.md
@@ -1,1 +1,6 @@
 # Работа со сторибуком
+
+* Для работы с запросами
+  используется [storybook-addon-mock](https://storybook-addon-mock.vercel.app/?path=/docs/docs-introduction--docs)
+* Для корректной работы с loki требуется динамически передавать url запроса полученного из обьекта window
+* Мок данные хранятся в директории __mocks__
--- loki.config.js
+++ loki.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  diffingEngine: 'looks-same',
   configurations: {
+    'chrome.iphone7': {
+      preset: 'iPhone 7',
+      target: 'chrome.docker',
+    },
     'chrome.laptop': {
+      deviceScaleFactor: 1,
+      height: 768,
+      mobile: false,
       target: 'chrome.docker',
       width: 1366,
-      height: 768,
-      deviceScaleFactor: 1,
-      mobile: false,
-    },
-    'chrome.iphone7': {
-      target: 'chrome.docker',
-      preset: 'iPhone 7',
     },
   },
+  diffingEngine: 'looks-same',
 };
