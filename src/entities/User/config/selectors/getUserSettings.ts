b38dbb9ca97b31b2e7824a55826f47a1320223bc
ViRybalkin <kinjeks@gmail.com>
2023-07-14 13:27:55 +0300

    feat: Добавлено приветственное модальное окно

--- src/entities/User/config/selectors/getUserSettings.ts
+++ src/entities/User/config/selectors/getUserSettings.ts
@@ -2,4 +2,4 @@ import {createSelector} from '@reduxjs/toolkit';
 import {UserSliceTypes} from '../types/UserSlice.types';
 import {getUser} from './getUser';
 
-export const getUserSettings = createSelector(getUser, (state: UserSliceTypes) => state?.userSettings);
+export const getUserSettings = createSelector(getUser, (state: UserSliceTypes) => state?.userSettings ?? {});
